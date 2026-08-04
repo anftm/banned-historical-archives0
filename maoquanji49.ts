1444b4a22e908460b947aa9a62ca38bb37d72b41
anftm <3716761@qq.com>
2026-08-05 01:17:49 +0800

    校订 中央军委转发五十四军对反对派进行工作情况报告的批语

--- maoquanji49.ts
+++ maoquanji49.ts
@@ -2785,18 +2785,24 @@ export default {
         "page_start": 481,
         "page_end": 482,
         "title": "中央军委转发五十四军对反对派进行工作情况报告的批语",
-        "dates": [
-          {
-            "year": 1967,
-            "month": 5,
-            "day": 27
-          }
-        ],
+        "dates": [],
         "is_range_date": false,
-        "authors": [
-          "毛泽东"
-        ]
-      },
+        "authors": []
+      ,
+        "tags": [
+        {
+          "name": "毛泽东",
+          "type": "人物"
+        },
+        {
+          "name": "林彪",
+          "type": "人物"
+        },
+        {
+          "name": "重庆文革",
+          "type": "人物"
+        }
+      ]},
       {
         "page_start": 483,
         "page_end": 483,
